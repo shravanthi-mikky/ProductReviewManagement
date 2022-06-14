6ef622cad001ae0089e7f8a807591a51b8bc23b6
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-06-14 18:35:40 +0530

    [shravanthi] - Retieve ProductID and Review using select

--- Product_Review/Program.cs
+++ Product_Review/Program.cs
@@ -58,7 +58,7 @@ List<ProductReview> productReviewList2 = new List<ProductReview>()
             };
 while (true)
 {
-    Console.WriteLine("Choose the correct option : \n1Add Values to List\n2)Top 3 Records\n3)Get Records of Rating > 3 from ProductId's 1/4/9\n4)Count of ProductID and GroupBy ProductID\n5)Retieve ProductID and Review\n6)Skip Top 5 records\n7)Create a DataTable\n8)Get Records Where Islike Is True\n9)Average of Ratings by ProductId\n10)Get Records where Review = Nice\n11)Get Records Order By Rating For UserId 10");
+    Console.WriteLine("Choose the correct option : \n1Add Values to List\n2)Top 3 Records\n3)Get Records of Rating > 3 from ProductId's 1/4/9\n4)Count of ProductID and GroupBy ProductID\n5)Retieve ProductID and Review\n6)Skip Top 5 records\n7)Create a DataTable\n8)Get Records Where Islike Is True\n9)Average of Ratings by ProductId\n10)Get Records where Review = Nice\n11)Get Records Order By Rating For UserId 10\n12)Retrieve ProductId And Review using Select");
     int option = Convert.ToInt16(Console.ReadLine());
     switch (option)
     {
@@ -101,6 +101,9 @@ while (true)
             break;
         case 11:
             productReviewMethods.OrderByRatingForUserId10(productReviewList);
+            break;
+        case 12:
+            productReviewMethods.RetrieveProductIdAndReview(productReviewList);
             break;
         default:
             Console.WriteLine("Please choose the correct option!");
